9ec256a5488d85fd479122b43d9c4e402fb994ea
Danilo Quintal <danjquintal@gmail.com>
2025-11-06 18:41:55 +0000

    entrega da aula 3 e ficheiro info.js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # A3_Entrega
 trabalho para entrega dia 06/11/2025
+"O meu primeiro commit do módulo." Danilo Quintal
---
+++ info.js
@@ -0,0 +1,4 @@
+console.log("A minha comida preferida é bolo de viena")
+console.log("A minha bebida preferida é galão")
+console.log("A minha cor preferida é preto")
+console.log("O meu hobbie é sim racing")
